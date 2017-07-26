058f4b0f5973836b521c8dffbc08a7c6fd51770f
Wolfgang Becker <wolli.becker@gmail.com>
2017-07-26 19:50:07 +0200

    factor out recipe-item template from recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,6 +12,11 @@ export class RecipeListComponent implements OnInit {
       'A Test Recipe',
       'This is a test',
       'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'
+    ),
+    new Recipe(
+      'Another Recipe',
+      'Another test',
+      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'
     )
   ];
 
