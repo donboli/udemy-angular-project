4510d75f2e1e4e0420bdd39768016830e0a56bff
Wolfgang Becker <wolli.becker@gmail.com>
2017-08-30 16:44:02 -0500

    Enable recipe deletion

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -3,7 +3,7 @@ import {
   OnInit,
   Input
 } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { Recipe } from '../recipe.model';
 import { ShoppingListService } from '../../shopping-list/shopping-list.service';
@@ -20,7 +20,8 @@ export class RecipeDetailComponent implements OnInit {
 
   constructor(
     private recipeService: RecipeService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -32,5 +33,10 @@ export class RecipeDetailComponent implements OnInit {
 
   addToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  deleteRecipe() {
+    this.recipeService.removeRecipe(this.id);
+    this.router.navigate(['..'], { relativeTo: this.route });
   }
 }
