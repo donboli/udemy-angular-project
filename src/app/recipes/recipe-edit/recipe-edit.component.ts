56c8559f4777c62a2ae71d2b40af79cd066d2866
Wolfgang Becker <wolli.becker@gmail.com>
2017-08-30 16:56:18 -0500

    Navigate to recipes after editing a recipe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -37,6 +37,8 @@ export class RecipeEditComponent implements OnInit {
     } else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+
+    this.navigateToRecipes();
   }
 
   onAddIngredient() {
@@ -51,7 +53,7 @@ export class RecipeEditComponent implements OnInit {
     );
   }
 
-  cancel() {
+  navigateToRecipes() {
     this.router.navigate(['..'], { relativeTo: this.route });
   }
 
