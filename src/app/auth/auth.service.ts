794469cd48b08c67a16f4ccc1201b4e734d5e557
Wolfgang Becker <wolli.becker@gmail.com>
2017-09-13 19:46:04 +0200

    Add logout button to header

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -29,6 +29,11 @@ export class AuthService {
       );
   }
 
+  logout() {
+    firebase.auth().signOut();
+    this.token = null;
+  }
+
   getToken() {
     firebase.auth().currentUser.getToken()
       .then(
