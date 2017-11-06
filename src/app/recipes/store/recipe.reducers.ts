af879415d7fa0c7311a98b1368670dd36a96f7cf
Wolfgang Becker <wolli.becker@gmail.com>
2017-11-06 22:03:31 +0100

    Add store reducers and actions for the recipes section

---
+++ src/app/recipes/store/recipe.reducers.ts
@@ -0,0 +1,72 @@
+import { Recipe } from '../recipe.model';
+import { Ingredient } from '../../shared/ingredient.model';
+import * as RecipeActions from './recipe.actions';
+
+export interface FeatureState {
+  recipes: State;
+}
+
+export interface State {
+  recipes: Recipe[];
+}
+
+const initialState: State = {
+  recipes: [
+    new Recipe(
+      'Tasty Schnitzel',
+      'This is a test',
+      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ),
+    new Recipe(
+      'Big Fat Burger',
+      'Another test',
+      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
+    )
+  ]
+};
+
+
+export function recipeReducer(state = initialState, action: RecipeActions.RecipeActions) {
+  switch (action.type) {
+    case RecipeActions.SET_RECIPES:
+      return {
+        ...state,
+        recipes: [...action.payload]
+      };
+    case RecipeActions.ADD_RECIPE:
+      return {
+        ...state,
+        recipes: [
+          ...state.recipes,
+          action.payload
+        ]
+      };
+    case RecipeActions.UPDATE_RECIPE:
+      return {
+        ...state,
+        recipes: [
+          ...state.recipes.slice(0, action.payload.index),
+          action.payload.updatedRecipe,
+          ...state.recipes.slice(action.payload.index)
+        ]
+      };
+    case RecipeActions.DELETE_RECIPE:
+      return {
+        ...state,
+        recipes: [
+          ...state.recipes.slice(0, action.payload),
+          ...state.recipes.slice(action.payload)
+        ]
+      };
+    default:
+      return state;
+  }
+}
